df2361a8c24485be089904da486dac4202d76639
fabio fumarola <fabiofumarola@gmail.com>
2018-01-11 19:04:35 +0100

    fixes #26

---
+++ opencivitas/openbilanci_extractor.py
@@ -0,0 +1,76 @@
+#!/bin/python
+
+import urllib.request, json
+import numpy as np
+import pandas as pd
+import logging
+
+from random import randint
+from time import sleep
+
+
+def init_logger():
+    # create logger
+    logger = logging.getLogger('openbilanci-extractor')
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
+
+
+def extract_data(df, logger):
+    d_types = ['entrate', 'spese']
+    base_url = 'https://www.openbilanci.it/armonizzati/bilanci/%s-comune-%s/%s/dettaglio.json?year=2016&type=preventivo'
+
+    missed_urls = []
+
+    i = 0
+    logger.info('extracting %d from opencivitas dataset' % df.shape[0])
+    for r in df.values:
+        cod, city, year, province = r
+        if city is not 'Nan':
+            city = city.lower().replace(' ', '-')
+            province = province.lower()
+            city_url = base_url % (city, province, d_types[0])
+
+            filename = './data/bilanci/%s_%s.json' % (cod, city)
+
+            try:
+                sleep(randint(1, 10))
+                i += 1
+                logger.info('processing url %s', city_url)
+                with urllib.request.urlopen(city_url) as url:
+                    data = json.loads(url.read().decode())
+
+                    with open(filename, 'w') as o:
+                        json.dump(data, o)
+
+                        logger.info('saved data into %s' % filename)
+
+            except Exception as ex:
+                logger.error('error %s for url %s' % (ex, city_url))
+                missed_urls.append(city_url)
+
+    logger.info("finished extraction for %d urls" % i)
+    with open("./data/bilanci/error_url.txt") as o:
+        o.write(','.join(missed_urls))
+
+
+logger = init_logger()
+
+df_sose = pd.read_csv('./data/opencivitas-spesa-storica+dati-irpef.csv')
+columns = ['COMUNE_CAT_COD', 'Denominazione Comune', 'ANNO', 'Sigla Provincia_y']
+df = df_sose[columns]
+
+extract_data(df, logger)
